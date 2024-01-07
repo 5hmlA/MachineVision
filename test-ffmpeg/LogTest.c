57afe84a0641c390c0f8d949ce283f975a26ca38
ZuYun <jonas.jzy@gmail.com>
2024-01-07 17:00:50 +0800

    🎓

--- test-ffmpeg/LogTest.c
+++ test-ffmpeg/LogTest.c
@@ -4,12 +4,21 @@
 
 int main() {
     // 定义一个字符串
-    char myString[] = "Hello, World!";
-
+    char *video = "C:\\Users\\xuchuqi\\CLionProjects\\MachineVision\\test_video.mp4";
     // 使用printf输出字符串
-    printf("%s\n", myString);
+    printf("%s\n", video);
     av_log_set_level(AV_LOG_DEBUG);
-    av_log(NULL, AV_LOG_WARNING, "000");
-    av_log(NULL, AV_LOG_INFO, "avformat_configurations: \n %s", avformat_configuration());
+//    av_log(NULL, AV_LOG_INFO, "avformat_configurations: \n %s", avformat_configuration());
+
+//test_video.mp4
+//
+    AVFormatContext *avFormatContext = NULL;
+    int ret = 0;
+    if ((ret = avformat_open_input(&avFormatContext, video, NULL, NULL))) {
+        av_log(NULL, AV_LOG_ERROR, "%s\n", av_err2str(ret));
+        exit(-1);
+    }
+    printf("------------------------");
+    free(avFormatContext);
     return 0;
 }
